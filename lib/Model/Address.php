ce9207b89cc5067abda4532948de3ecee4763f76
jiri.strzelecki <jiri.strzelecki@gmail.com>
2024-03-06 22:26:03 +0100

    Supress 8.1 deprecation notices

--- lib/Model/Address.php
+++ lib/Model/Address.php
@@ -804,7 +804,7 @@ class Address implements ModelInterface, ArrayAccess
      *
      * @return boolean
      */
-    public function offsetExists($offset)
+    public function offsetExists($offset): bool
     {
         return isset($this->container[$offset]);
     }
@@ -816,7 +816,7 @@ class Address implements ModelInterface, ArrayAccess
      *
      * @return mixed
      */
-    public function offsetGet($offset)
+    public function offsetGet($offset): mixed
     {
         return isset($this->container[$offset]) ? $this->container[$offset] : null;
     }
@@ -829,7 +829,7 @@ class Address implements ModelInterface, ArrayAccess
      *
      * @return void
      */
-    public function offsetSet($offset, $value)
+    public function offsetSet($offset, $value): void
     {
         if (is_null($offset)) {
             $this->container[] = $value;
@@ -845,7 +845,7 @@ class Address implements ModelInterface, ArrayAccess
      *
      * @return void
      */
-    public function offsetUnset($offset)
+    public function offsetUnset($offset): void
     {
         unset($this->container[$offset]);
     }
